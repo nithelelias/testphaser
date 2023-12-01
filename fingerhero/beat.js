88e9d747c5ac1f3aad507977cf253dad925c72da
Nithel elias <nithel.elias@pragma.com.co>
2023-11-30 20:18:47 -0500

    finger hero

---
+++ fingerhero/beat.js
@@ -0,0 +1,188 @@
+const default_options = {
+  filterFrequency: 100,
+  peakGain: 15,
+  threshold: 0.8,
+  sampleSkip: 350,
+  minAnimationTime: 0.4,
+};
+export default class BeatBeat {
+  isPlaying = false;
+
+  /**
+   * offlineContext!: OfflineAudioContext
+   */
+  offlineContext;
+  /**
+   *  buffer!: AudioBuffer
+   */
+  buffer;
+
+  channels = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+  };
+
+  constructor(
+    context, // AudioContext
+    name,
+    options = default_options
+  ) {
+    let _options = { ...default_options, ...options };
+    for (let prop in _options) {
+      this[prop] = _options[prop];
+    }
+    this.context = context;
+    this.name = name;
+  }
+
+  load() {
+    return new Promise(async (resolve) => {
+      const resp = await fetch(this.name);
+      const file = await resp.arrayBuffer();
+      this.context.decodeAudioData(file, async (buffer) => {
+        this.buffer = buffer;
+        await this.analyze();
+        resolve();
+      });
+    });
+  }
+
+  play() {
+    this.isPlaying = true;
+    let pausedAt = 0;
+    let source;
+    let _play = () => {
+      source = this.context.createBufferSource();
+      source.buffer = this.buffer;
+      source.connect(this.context.destination);
+      console.log(pausedAt);
+      source.start(0, pausedAt);
+    };
+    _play();
+    return {
+      source,
+      play: _play,
+      pause: () => {
+        source.stop(0);
+        pausedAt = source.context.currentTime;
+      },
+    };
+  }
+
+  async analyze() {
+    await this.analyzeFilter(0);
+    await this.analyzeFilter(1);
+    await this.analyzeFilter(2);
+    await this.analyzeFilter(3);
+  }
+
+  async analyzeFilter(channelIdx) {
+    const offlineContext = new OfflineAudioContext(
+      1,
+      this.buffer.length,
+      this.buffer.sampleRate
+    );
+
+    const source = offlineContext.createBufferSource();
+    source.buffer = this.buffer;
+    if (channelIdx === 0) {
+      this.getLowFilter(source, offlineContext);
+    }
+
+    if (channelIdx === 1) {
+      this.getMidLowFilter(source, offlineContext);
+    }
+    if (channelIdx === 2) {
+      this.getHighFilter(source, offlineContext);
+    }
+    if (channelIdx === 3) {
+      this.getMidHighFilter(source, offlineContext);
+    }
+
+    source.start();
+    const buffer = await offlineContext.startRendering();
+
+    const data = buffer.getChannelData(0);
+    let songData = [];
+    for (let i = 0; i < data.length; ++i) {
+      if (data[i] > this.threshold) {
+        const time = i / buffer.sampleRate;
+        const previousTime = songData.length
+          ? songData[songData.length - 1].time
+          : 0;
+        if (time - previousTime > this.minAnimationTime) {
+          songData.push({
+            data: data[i],
+            time,
+          });
+        }
+      }
+      i += this.sampleSkip;
+    }
+    this.channels[channelIdx] = songData;
+  }
+  getLowFilter(source, offlineContext) {
+    const filter = offlineContext.createBiquadFilter();
+    filter.type = "lowpass";
+    filter.frequency.value = 1000;
+    filter.Q.value = 1;
+
+    const filter2 = offlineContext.createBiquadFilter();
+    filter2.type = "peaking";
+    filter2.frequency.value = 1000;
+    filter2.Q.value = 1;
+    filter2.gain.value = this.peakGain;
+
+    source.connect(filter2);
+    filter2.connect(filter);
+    filter.connect(offlineContext.destination);
+  }
+  getMidLowFilter(source, offlineContext) {
+    const filter = offlineContext.createBiquadFilter();
+    filter.type = "allpass";
+    filter.gain.value = 0;
+    const filter2 = offlineContext.createBiquadFilter();
+    filter2.type = "peaking";
+    filter2.frequency.value = 720;
+    filter2.Q.value = 1;
+    filter2.gain.value = this.peakGain;
+
+    source.connect(filter2);
+    filter2.connect(filter);
+    filter.connect(offlineContext.destination);
+  }
+  getMidHighFilter(source, offlineContext) {
+    const filter = offlineContext.createBiquadFilter();
+    filter.type = "highpass";
+    filter.frequency.value = 440;
+    filter.Q.value = 0;
+
+    const filter2 = offlineContext.createBiquadFilter();
+    filter2.type = "peaking";
+    filter2.frequency.value = 320;
+    filter2.Q.value = 1;
+    filter2.gain.value = this.peakGain;
+
+    source.connect(filter2);
+    filter2.connect(filter);
+    filter.connect(offlineContext.destination);
+  }
+  getHighFilter(source, offlineContext) {
+    const filter = offlineContext.createBiquadFilter();
+    filter.type = "highpass";
+    filter.frequency.value = 100;
+    filter.Q.value = 1;
+
+    const filter2 = offlineContext.createBiquadFilter();
+    filter2.type = "peaking";
+    filter2.frequency.value = 100;
+    filter2.Q.value = 1;
+    filter2.gain.value = this.peakGain;
+
+    source.connect(filter2);
+    filter2.connect(filter);
+    filter.connect(offlineContext.destination);
+  }
+}
--- fingerhero/scene/main.js
+++ fingerhero/scene/main.js
@@ -1,8 +1,34 @@
 import ASSET_MAPTILE from "../assets/maptile.js";
+import BeatBeat from "../beat.js";
 const rail_colors = [0xff0000, 0x00ff00, 0x0000ff, 0xfff000, 0x00ffff];
 const rail_border_color = 0xffffff;
+const control_directions = ["left", "down", "right", "up"];
+const song_name="MYKKA"
 function getTimeNow() {
   return Main.current.time.now;
+}
+function addClickListenerAt(
+  scene,
+  gameObject,
+  onClick,
+  onPress = () => null,
+  onRelease = () => null
+) {
+  const ondown = () => {
+    onPress();
+    gameObject.once("pointerup", () => {
+      onClick();
+    });
+    scene.input.once("pointerup", () => {
+      onRelease();
+    });
+  };
+  gameObject.setInteractive();
+  gameObject.on("pointerdown", ondown);
+
+  return () => {
+    gameObject.off("pointerdown", ondown);
+  };
 }
 class RailSprite extends Phaser.GameObjects.Container {
   progress = 1; //0 -> 1
@@ -83,20 +109,13 @@ class RailSprite extends Phaser.GameObjects.Container {
 }
 class Track extends Phaser.GameObjects.Container {
   started = false;
-  constructor(scene, rails) {
+  constructor(scene, rails, onChallenge = () => null) {
     super(scene, 0, 0, []);
     scene.add.existing(this);
     this.rails = rails;
     this.track = [];
+    this.onChallenge = onChallenge;
     this.startTime = 0;
-
-    // });
-
-    this.caption = scene.add.text(16, 16, "HOLA MUNDO", {
-      fill: "#7fdbff",
-      fontFamily: "monospace",
-      lineSpacing: 4,
-    });
   }
   addOne() {
     let one = new RailSprite(this.scene, "dot", {
@@ -107,45 +126,70 @@ class Track extends Phaser.GameObjects.Container {
     one.startTime = 0;
     one.endTime = 0;
     one.total_time = 1000; // one second
-    one.active = false;
+    one.idle = true;
 
     one.putAtRail = (_railIdx) => {
       let _rail = {
         start: this.rails.start[_railIdx],
         end: this.rails.end[_railIdx],
       };
-      console.log(_rail);
+      one._railIdx = _railIdx;
       one.setRail(_rail);
     };
     one.setSeconds = (_seconds = 1) => {
-      one.total_time = _seconds * 1000;
+      one.total_time = _seconds > 900 ? _seconds : _seconds * 1000;
     };
     one.stop = () => {
-      one.active = false;
+      one.idle = true;
       one.setVisible(false);
     };
     one.start = (_seconds) => {
       if (_seconds) {
         one.setSeconds(_seconds);
       }
-      one.startTime = getTimeNow();
-      one.endTime = getTimeNow() + one.total_time;
-      one.setVisible(true);
-      one.active = true;
-    };
-    one.updateProgressTime = (_time, _delta) => {
-      if (!one.active) {
-        return;
-      }
-      console.log(_time, _delta);
-      let dif_time = _time - one.startTime;
-      let progress = dif_time / one.total_time;
-      one.setProgress(progress);
-      if (one.progress >= 1.2) {
-        console.log("MISS!");
+      let eventName = "press-rail-" + one._railIdx;
+      let tween;
+      let unbind;
+      const holderProgress = {
+        t: 0,
+      };
+      const onPressRail = () => {
+        if (holderProgress.t < 0.9) {
+          this.onChallenge(0);
+          return;
+        }
+        if (holderProgress.t < 1) {
+          this.onChallenge(1);
+        } else {
+          this.onChallenge(2);
+        }
+        end();
+      };
+      const end = () => {
+        tween.stop();
         one.stop();
-      }
+        this.scene.events.off(eventName, onPressRail, this.scene);
+        //tween.destroy();
+      };
+      this.scene.events.on(eventName, onPressRail, this.scene);
+
+      one.idle = false;
+      tween = this.scene.add.tween({
+        targets: holderProgress,
+        t: 1.2,
+        duration: one.total_time,
+        onUpdate: () => {
+          one.setVisible(true);
+          one.setProgress(holderProgress.t);
+
+          if (one.progress >= 1.2) {
+            console.log("MISS!");
+            end();
+          }
+        },
+      });
     };
+
     one.stop();
     this.add(one);
     return one;
@@ -153,7 +197,7 @@ class Track extends Phaser.GameObjects.Container {
 
   getOne() {
     for (let i in this.list) {
-      if (!this.list[i].active) {
+      if (this.list[i].idle) {
         return this.list[i];
       }
     }
@@ -163,7 +207,7 @@ class Track extends Phaser.GameObjects.Container {
   getTotalActives() {
     let total = 0;
     for (let i in this.list) {
-      if (this.list[i].active) {
+      if (!this.list[i].idle) {
         total++;
       }
     }
@@ -175,12 +219,31 @@ class Track extends Phaser.GameObjects.Container {
   start() {
     this.startTime = getTimeNow();
     this.started = true;
+    let durationSeconds = 2000;
+    let parsedTrack = this.track.map((_section) => {
+      return {
+        at: _section.t,
+        run: () => {
+          for (let i in _section.k) {
+            let one = this.getOne();
+            let rail_idx = _section.k[i];
+
+            one.putAtRail(rail_idx);
+            one.start(durationSeconds);
+          }
+        },
+      };
+    });
+
+    const timeline = this.scene.add.timeline(parsedTrack);
+
+    timeline.play();
   }
   update(time, delta) {
     if (!this.started) {
       return;
     }
-    let timestamp = getTimeNow() - this.startTime;
+    /*  let timestamp = getTimeNow() - this.startTime;
     let wait_t = -1;
     let total_expected = this.track.length;
     if (total_expected > 0) {
@@ -199,9 +262,7 @@ class Track extends Phaser.GameObjects.Container {
         //this.track.push(section);
       }
     }
-    this.caption.setText(`TIME:${timestamp} 
-    WAIT:${wait_t}
-    `);
+    
     let total = this.getTotalActives();
     if (total > 0 || total_expected > 0) {
       this.list.forEach((_one) => {
@@ -210,34 +271,13 @@ class Track extends Phaser.GameObjects.Container {
     } else {
       this.started = false;
       console.log("ended");
-    }
+    } */
   }
 }
-function addClickListenerAt(
-  scene,
-  gameObject,
-  onClick,
-  onPress = () => null,
-  onRelease = () => null
-) {
-  const ondown = () => {
-    onPress();
-    gameObject.once("pointerup", () => {
-      onClick();
-    });
-    scene.input.once("pointerup", () => {
-      onRelease();
-    });
-  };
-  gameObject.setInteractive();
-  gameObject.on("pointerdown", ondown);
 
-  return () => {
-    gameObject.off("pointerdown", ondown);
-  };
-}
 export default class Main extends Phaser.Scene {
   arrow_actives = [false, false, false, false];
+  started = false;
   static current;
   constructor() {
     super({
@@ -256,22 +296,48 @@ export default class Main extends Phaser.Scene {
       frameWidth: ASSET_MAPTILE.sprite_size,
       frameHeight: ASSET_MAPTILE.sprite_size,
     });
+    this.load.audio("music", "./assets/music/"+song_name+".mp3");
   }
   create() {
     Main.current = this;
     window.main = this;
+    this.points = 0;
+    this.started = false;
     this.initRails();
+    this.started = true;
     this.createLines();
     this.createDownArrows();
     this.createKeyInputListener();
-    this.track = new Track(this, this.rails);
-    this.startLevel();
+    this.track = new Track(this, this.rails, (_challengScore) => {
+      this.points = Math.max(0, this.points + _challengScore);
+    });
+    this.caption = this.add.text(16, 16, "HOLA MUNDO", {
+      fill: "#7fdbff",
+      fontFamily: "monospace",
+      lineSpacing: 4,
+    });
+    this.createIntroStart();
   }
+  createIntroStart() {
+    let introText = this.add
+      .text(this.scale.width / 2, this.scale.height / 2, "click to start", {
+        fill: "#7fdbff",
+        fontFamily: "monospace",
+        lineSpacing: 4,
+      })
+      .setOrigin(0.5);
+    this.input.once("pointerdown", () => {
+      introText.destroy();
+
+      this.startLevel();
+    });
+  }
+
   initRails() {
     let steps = parseInt(this.scale.width / 4);
 
     let center_x = parseInt(this.scale.width / 2);
-    let start_y = 200;
+    let start_y = 16;
     let end_y = this.scale.height - 130;
     let railsX = [
       parseInt(steps * 0.5),
@@ -366,24 +432,12 @@ export default class Main extends Phaser.Scene {
     );
   }
   createDownArrows() {
-    const arrows = [
-      new RailSprite(this, "arrow_left", {
-        start: this.rails.start[0],
-        end: this.rails.end[0],
-      }),
-      new RailSprite(this, "arrow_up", {
-        start: this.rails.start[1],
-        end: this.rails.end[1],
-      }),
-      new RailSprite(this, "arrow_down", {
-        start: this.rails.start[2],
-        end: this.rails.end[2],
-      }),
-      new RailSprite(this, "arrow_right", {
-        start: this.rails.start[3],
-        end: this.rails.end[3],
-      }),
-    ];
+    const arrows = control_directions.map((dir, idx) => {
+      return new RailSprite(this, "arrow_" + dir, {
+        start: this.rails.start[idx],
+        end: this.rails.end[idx],
+      });
+    });
 
     arrows.forEach((_arrow, _idx) => {
       _arrow.disabled();
@@ -418,14 +472,15 @@ export default class Main extends Phaser.Scene {
   triggerRailAt(idx, state = true) {
     //console.log("trigger rail at ", idx);
     this.arrow_actives[idx] = state;
+    this.events.emit("press-rail-" + idx);
   }
 
   createKeyInputListener() {
     const cursors = this.input.keyboard.createCursorKeys();
-    const directions = ["left", "up", "down", "right"];
+
     const validate = () => {
-      for (let i in directions) {
-        let dir = directions[i];
+      for (let i in control_directions) {
+        let dir = control_directions[i];
         this.triggerRailAt(i, cursors[dir].isDown);
       }
     };
@@ -437,44 +492,51 @@ export default class Main extends Phaser.Scene {
       this.input.keyboard.off("keyup", validate);
     };
   }
-  startLevel() {
-    console.log("start level");
-    this.track.setTrack([
+  async startLevel() {
+    const sound = new BeatBeat(
+      new AudioContext(),
+      "./assets/music/"+song_name+".mp3",
       {
-        t: 2000,
-        k: [0],
-      } /* ,
-      {
-        t: 2500,
-        k: [1],
-      },
-      {
-        t: 3000,
-        k: [2],
-      },
-      {
-        t: 3500,
-        k: [3],
-      },
-      {
-        t: 4000,
-        k: [1],
-      },
-      {
-        t: 4550,
-        k: [2],
-      },
-      {
-        t: 5000,
-        k: [1],
-      }, */,
-    ]);
+        filterFrequency: 1000,
+        threshold: 0.5,
+      }
+    );
+    await sound.load();
+    let parsedTrack = [];
+    let firstEntryNote = -1;
+    const mapDatSound = (channelIdx) => {
+      sound.channels[channelIdx].forEach((data) => {
+        if (firstEntryNote === -1 || data.time < firstEntryNote) {
+          firstEntryNote = data.time;
+        }
+        parsedTrack.push({
+          t: data.time * 1000,
+          k: [channelIdx],
+        });
+      });
+    };
+    mapDatSound(0);
+    mapDatSound(1);
+    mapDatSound(2);
+    mapDatSound(3);
+    let delay = firstEntryNote * 1000 + 0;
+    console.log("delay", delay);
+
+    this.track.setTrack(parsedTrack);
     this.track.start();
+    let music = this.sound.add("music");
+    setTimeout(() => {
+      music.play();
+    }, delay);
   }
   update(time, delta) {
+    if (!this.started) {
+      return;
+    }
     this.arrow_actives.forEach((active, idx) => {
       this.arrows[idx].setActive(active);
     });
     this.track.update(time, delta);
+    this.caption.setText(` points: ${this.points}`);
   }
 }
